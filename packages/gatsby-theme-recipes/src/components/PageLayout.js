79c229fbbdb68ad3966dd29c6c2e749f0809457c
Marinda <hello@marinda.me>
2019-09-24 19:06:47 +0200

    Local fonts :adding Cardo font

--- packages/gatsby-theme-recipes/src/components/PageLayout.js
+++ packages/gatsby-theme-recipes/src/components/PageLayout.js
@@ -33,6 +33,13 @@ const PageLayout = ({ children }) => {
           },
           "*": {
             boxSizing: "border-box"
+          },
+          "@font-face": {
+            fontFamily: "Cardo",
+            src: 'url("/fonts/cardo-regular.woff2")',
+            fontDisplay: "swap",
+            fontWeight: 400,
+            fontStyle: "normal"
           }
         }}
       />
