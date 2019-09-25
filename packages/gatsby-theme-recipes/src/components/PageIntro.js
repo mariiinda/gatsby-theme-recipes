baf0d943667b06a87a376d54cdd5adeef5604b2b
Marinda <hello@marinda.me>
2019-09-25 21:52:25 +0200

    Update PageIntro.js

--- packages/gatsby-theme-recipes/src/components/PageIntro.js
+++ packages/gatsby-theme-recipes/src/components/PageIntro.js
@@ -9,6 +9,8 @@ const PageIntro = ({ children }) => {
         css`
           text-align: center;
           padding: 0 0 ${theme.space[3]}px;
+          text-align: center;
+          margin: 0 auto ${theme.space[3]}px;
         `
       }
     >
