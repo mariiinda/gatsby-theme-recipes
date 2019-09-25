1ee75a03953d0e8342738fe58279bd873a95da6d
Marinda <hello@marinda.me>
2019-09-25 23:07:39 +0200

    Update gatsby-config.js

--- packages/gatsby-theme-recipes/gatsby-config.js
+++ packages/gatsby-theme-recipes/gatsby-config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = ({
   basePath = "/",
-  contentPath = "recipes",
+  contentPath = "posts",
   iconPath,
   title = "Delicious Recipes",
   shortTitle = "Recipes"
